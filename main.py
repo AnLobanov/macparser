b144d460c8a748fe1d1b7f7c4670bffeb5b5944d
Anton Lobanov <antnlobanov@gmail.com>
2023-08-05 01:32:38 +0500

    Check directories and RAM modules

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-import zipfile, os, json, shutil
+import os, json
 OK = '\033[92m'  # Colors for terminal
 FAIL = '\033[91m'
 END = '\033[0m'
-currentpath = os.getcwd() + '/'
-if not os.path.exists(currentpath + 'temp'):
-    os.mkdir(currentpath + 'temp')
-try:
-    with zipfile.ZipFile(input('File name (file should be located in current path): '), 'r') as zf:
-        zf.extractall(currentpath + 'temp')
-except FileNotFoundError:
-    print('The file was not found. Check the file name and its extension and try again')
-    os._exit(1)
-requiredRam = int(input('How many GB of RAM every server needs: '))
+currentpath = os.getcwd()
+requiredRam = int(input('How many RAM modules every server needs: '))
 print()
 problems = []
-for path in os.listdir(currentpath + 'temp'):
-    with open(currentpath + 'temp/' + path + '/outputs/identify.json') as jsonfile:
-        data = json.load(jsonfile)  # Data loading
-        ram = int(data['memory']['total'].replace(' GB', ''))
-        if ram < requiredRam:  # Compare RAM
-            print(path.replace('%3A', ':') + ' ' + FAIL + str(ram) + ' GB' + END)
-            problems.append(path.replace('%3A', ':'))
-        else:
-            print(path.replace('%3A', ':') + ' ' + OK + str(ram) + ' GB' + END)
-shutil.rmtree(currentpath + 'temp')  # Delete temp directory
+for pathname in os.listdir(currentpath):
+    if os.path.isdir(currentpath + '/' + pathname) and "%3A" in pathname:
+        with open(currentpath + '/' + pathname + '/outputs/identify.json') as jsonfile:
+            data = json.load(jsonfile)  # Data loading
+            ram = int(data['memory']['count'])
+            if ram < requiredRam:  # Compare RAM
+                print(pathname.replace('%3A', ':') + ' ' + FAIL + str(ram) + ' pcs' + END)
+                problems.append(pathname.replace('%3A', ':'))
+            else:
+                print(pathname.replace('%3A', ':') + ' ' + OK + str(ram) + ' pcs' + END)
 if problems == []:
     print('\n\nEverything is OK')
 else:
-    print('\n\n' + str(len(problems)) + " hosts don't have enough RAM:")
+    print('\n\n' + str(len(problems)) + " hosts don't have enough RAM modules:")
     for host in problems:
         print(host)
