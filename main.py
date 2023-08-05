0ffc0841789f16cafff6bb0677f38b04aba77449
Anton Lobanov <antnlobanov@gmail.com>
2023-08-05 15:47:47 +0500

    Added server names

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ for pathname in os.listdir(currentpath):
         with open(currentpath + '/' + pathname + '/outputs/identify.json') as jsonfile:
             data = json.load(jsonfile)  # Data loading
             ram = int(data['memory']['count'])
+            node = data['meta']['node']
             if ram < requiredRam:  # Compare RAM
-                print(pathname.replace('%3A', ':') + ' ' + FAIL + str(ram) + ' pcs' + END)
-                problems.append(pathname.replace('%3A', ':'))
+                print(node + ' ' + pathname.replace('%3A', ':') + ' ' + FAIL + str(ram) + ' pcs' + END)
+                problems.append([node, pathname.replace('%3A', ':')])
             else:
-                print(pathname.replace('%3A', ':') + ' ' + OK + str(ram) + ' pcs' + END)
+                print(node + ' ' + pathname.replace('%3A', ':') + ' ' + OK + str(ram) + ' pcs' + END)
 if problems == []:
     print('\n\nEverything is OK')
 else:
     print('\n\n' + str(len(problems)) + " hosts don't have enough RAM modules:")
     for host in problems:
-        print(host)
+        print(host[0], host[1])
